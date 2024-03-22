79b4498b738ab6ad8844350849d3dadd5067d6f3
Gramcam <121652975+Gramcam@users.noreply.github.com>
2024-03-22 11:23:45 -0600

    Add files via upload

---
+++ GrogueTheSecondOne/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GrogueTheSecondOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstPlayArea = new ListBox();
+            btnTestChange = new Button();
+            SuspendLayout();
+            // 
+            // lstPlayArea
+            // 
+            lstPlayArea.BackColor = Color.Black;
+            lstPlayArea.Font = new Font("Consolas", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lstPlayArea.ForeColor = Color.DarkOrange;
+            lstPlayArea.ItemHeight = 32;
+            lstPlayArea.Location = new Point(0, 0);
+            lstPlayArea.Name = "lstPlayArea";
+            lstPlayArea.Size = new Size(755, 452);
+            lstPlayArea.TabIndex = 0;
+            // 
+            // btnTestChange
+            // 
+            btnTestChange.Location = new Point(888, 274);
+            btnTestChange.Name = "btnTestChange";
+            btnTestChange.Size = new Size(75, 23);
+            btnTestChange.TabIndex = 1;
+            btnTestChange.Text = "button1";
+            btnTestChange.UseVisualStyleBackColor = true;
+            btnTestChange.Click += btnTestChange_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1060, 612);
+            Controls.Add(btnTestChange);
+            Controls.Add(lstPlayArea);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox lstPlayArea;
+        private Button btnTestChange;
+    }
+}
---
+++ GrogueTheSecondOne/Form1.cs
@@ -0,0 +1,80 @@
+namespace GrogueTheSecondOne
+{
+    public partial class Form1 : Form
+    {
+        List<List<char>> playSpaceList = new List<List<char>>();
+        List<List<char>> OrigplaySpaceList = new List<List<char>>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load Level Data From Textfile
+
+            LoadLevel("Level_1.txt");
+
+
+        }
+
+        void LoadLevel(string lvl)
+        {
+            //Load from textfile
+            try
+            {
+                StreamReader inputFile = File.OpenText(lvl);
+
+                while (!inputFile.EndOfStream)
+                {
+                    lstPlayArea.Items.Add(inputFile.ReadLine());
+                }
+                inputFile.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Failed to retrieve level data!");
+            }
+
+            //Assign to nested lists to create a playgrid
+            foreach (string line in lstPlayArea.Items)
+            {
+                //Store each char within each line in a list
+                List<char> lineChars = new List<char>();
+                for (int col = 0; col < line.Length; col++)
+                {
+                    char readChar = line.ToString()[col];
+                    lineChars.Add(readChar);
+                }
+                //Add that row of chars to the PlaySpace list
+                playSpaceList.Add(lineChars);
+
+            }
+            OrigplaySpaceList = playSpaceList;
+
+        }
+
+        private void btnTestChange_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            Random rnd = new Random();
+
+            x = rnd.Next(1, playSpaceList[1].Count-1);
+            y = rnd.Next(1, playSpaceList.Count - 1);
+
+            playSpaceList[y][x] = 'n';
+
+            string newline = "";
+            //Get the Chars from the playspace, rebuild into a new string, 
+            //Place string into the listbox
+            for (x = 0; x < playSpaceList[y].Count; x++)
+            {
+                char readChar = playSpaceList[y][x];
+                newline += readChar;
+            }
+            
+            lstPlayArea.Items[y] = newline;
+
+        }
+    }
+}
