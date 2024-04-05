a1d48a42b5b359c6a090330519b5a964384109a8
Graham Campbell - Home <Graham.Campbell1@mymhc.ca>
2024-04-04 22:58:23 -0600

    Changed direcitons to enums.
    Fixed bug where standing still led to the environment overtaking mobs until they moved.
    
    "A rolling stone gathers no moss!" -Tiny

--- GrogueTheSecondOne/Form1.cs
+++ GrogueTheSecondOne/Form1.cs
@@ -5,14 +5,15 @@ namespace GrogueTheSecondOne
     public partial class Form1 : Form
     {
         //Map Fields
-        const int PLAYSPACEROWS = 12;
-        const int PLAYSPACECOL = 49;
-        public char[,] charOriginalMapState = new char[PLAYSPACEROWS, PLAYSPACECOL];
-        char[,] charPlayArea = new char[PLAYSPACEROWS, PLAYSPACECOL];
+        private const int PLAYSPACEROWS = 12;
+        private const int PLAYSPACECOL = 49;
+        private char[,] charOriginalMapState = new char[PLAYSPACEROWS, PLAYSPACECOL];
+        private char[,] charPlayArea = new char[PLAYSPACEROWS, PLAYSPACECOL];
+
+        private Random rnd = new Random();
 
         //List to store EnemyMobs
         List<mobEnemy> enemyList = new List<mobEnemy>();
-
         private mobEnemy playSpaceEnemy;
 
         public Form1()
@@ -23,38 +24,29 @@ namespace GrogueTheSecondOne
         private void Form1_Load(object sender, EventArgs e)
         {
             //Load Level Data From Textfile
-
             LoadLevel("Level_1.txt");
-
-            int x, y;
-            Random rnd = new Random();
-
-            //Choose random location that isnt populated
-            do
+            for (int i = 0; i < 3; i++)
             {
-                x = rnd.Next(1, PLAYSPACECOL - 1);
-                y = rnd.Next(1, PLAYSPACEROWS - 1);
+                int x, y;
+                
+                //Choose random location that isnt populated
+                do
+                {
+                    x = rnd.Next(1, PLAYSPACECOL - 1);
+                    y = rnd.Next(1, PLAYSPACEROWS - 1);
 
-            } while (charPlayArea[y, x] == 'N' || charPlayArea[y, x] == '#');
+                } while (charPlayArea[y, x] != '.');
 
-            playSpaceEnemy = new mobEnemy(y, x);
+                playSpaceEnemy = new mobEnemy(y, x);
 
-            charPlayArea[y, x] = playSpaceEnemy.Sprite;
-            enemyList.Add(playSpaceEnemy);
-
-            string newline = "";
-            //Get the Chars from the playspace, rebuild into a new string, 
-            //Place string into the listbox
-            for (x = 0; x < PLAYSPACECOL; x++)
-            {
-                char readChar = charPlayArea[y, x];
-                newline += readChar;
+                enemyList.Add(playSpaceEnemy);
             }
 
-            lstPlayArea.Items[y] = newline;
+            foreach (mobEnemy mob in enemyList)
+                UpdateEnemyRows(mob);
         }
 
-        void LoadLevel(string lvl)
+        private void LoadLevel(string lvl)
         {
             //Load from textfile
             try
@@ -90,12 +82,12 @@ namespace GrogueTheSecondOne
         {
             foreach (mobEnemy M in enemyList)
             {
-                M.moveUp(M);
+                M.MobMoveArrManip();
                 UpdateEnemyRows(M);
             }
         }
 
-        void UpdateEnemyRows(mobEnemy Enemy)
+        private void UpdateEnemyRows(mobEnemy Enemy)
         {
             string newline = "";
 
@@ -105,9 +97,10 @@ namespace GrogueTheSecondOne
             charPlayArea[Enemy.prevYLoc, Enemy.prevXloc] = charOriginalMapState[Enemy.prevYLoc, Enemy.prevXloc];
 
             //Redraw Original Map
+            //Use the GetLength method to select array dimension
             for (int g = 0; g < charOriginalMapState.GetLength(1); g++)
             {
-                newline += charOriginalMapState[Enemy.prevYLoc, g];
+                newline += charPlayArea[Enemy.prevYLoc, g];
             }
             lstPlayArea.Items[Enemy.prevYLoc] = newline;
 
--- GrogueTheSecondOne/mobEnemy.cs
+++ GrogueTheSecondOne/mobEnemy.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace GrogueTheSecondOne
 {
     internal class mobEnemy
     {
-        char asciiSprite;
-        int rowNum, colNum;
-        int prevRowNum, prevColNum;
+        private char asciiSprite;
+        private int rowNum, colNum;
+        private int prevRowNum, prevColNum;
 
+        //Movement Enums
+        public enum direction
+        {
+            NW, N, NE,
+            W, H, E,
+            SW, S, SE
+        }
+
+        private direction chosenDir;
         public int Xloc { get { return colNum; } }
         public int YLoc { get { return rowNum; } }
         public int prevXloc { get { return prevColNum; } }
@@ -31,7 +41,72 @@ namespace GrogueTheSecondOne
             prevRowNum = rowNum;
             M.rowNum -= 1;
         }
-    }
 
-   
+        public void MobMoveArrManip()
+        {
+            //Store enum names in array, get the length of the array
+            int enumCount = Enum.GetNames(typeof(direction)).Length;
+            Random rnd = new Random();
+
+            chosenDir = (direction)rnd.Next(0, enumCount++);
+
+
+            switch (chosenDir)
+            {
+                case direction.NW:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    rowNum--;
+                    colNum--;
+                    break;
+                case direction.N:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    rowNum--;
+                    break;
+                case direction.NE:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    rowNum--;
+                    colNum++;
+                    break;
+                case direction.W:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    colNum--;
+                    break;
+                case direction.H:
+                    break;
+                case direction.E:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    colNum++;
+                    break;
+                case direction.SW:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    rowNum++;
+                    colNum--;
+                    break;
+                case direction.S:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    rowNum++;
+                    break;
+                case direction.SE:
+                    prevColNum = colNum;
+                    prevRowNum = rowNum;
+                    rowNum--;
+                    colNum++;
+
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+
+    }
 }
